51c62a8317046fda69748a6cad0f5c5a8bf1ae14
Md Tawfiqul Islam <tawfiqmislam@gmail.com>
2025-08-05 13:59:13 +0600

    simple test endpoint to check responsiveness

--- main.py
+++ main.py
@@ -68,3 +68,7 @@ async def get_vector(file: UploadFile = File(...)):
 # if __name__ == "__main__":
 #     import uvicorn
 #     uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
+
+@app.get("/ping")
+async def ping():
+    return {"message": "pong"}
