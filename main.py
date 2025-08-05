15d6e90be6bdc7a0f2bb0e23261a4e477bb3f21f
Md Tawfiqul Islam <tawfiqmislam@gmail.com>
2025-08-05 14:39:19 +0600

    test changes

--- main.py
+++ main.py
@@ -63,12 +63,11 @@ async def get_vector(file: UploadFile = File(...)):
     vector = image_features[0].cpu().numpy().tolist()
     return JSONResponse(content={"vector": vector})
 
-
-
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
-
 @app.get("/ping")
 async def ping():
     return {"message": "pong"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
+
