50707f02ef208ad12de16b0d4a5e63907ed45526
Selim Reza <selimreza841@gmail.com>
2025-10-09 09:00:11 +0600

    refactor: clean up commented out code for uvicorn server

--- main.py
+++ main.py
@@ -80,10 +80,7 @@ async def search_vector(file: UploadFile = File(...)):
     return {"results": results}
 
 
+
 # if __name__ == "__main__":
 #     import uvicorn
-#     uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
+#     uvicorn.run("main:app", host="127.0.0.1", port=8001, reload=True)
